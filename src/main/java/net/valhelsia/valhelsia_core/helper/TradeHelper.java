184445d786e5963e97086445a21b010db8c80729
stal111 <stal111@gmx.de>
2021-08-05 17:53:11 +0200

    Add more trade methods

--- src/main/java/net/valhelsia/valhelsia_core/helper/TradeHelper.java
+++ src/main/java/net/valhelsia/valhelsia_core/helper/TradeHelper.java
@@ -1,7 +1,8 @@
 package net.valhelsia.valhelsia_core.helper;
 
+import net.minecraft.entity.merchant.villager.VillagerProfession;
 import net.minecraft.entity.merchant.villager.VillagerTrades;
-import net.minecraftforge.common.BasicTrade;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
 
 /**
@@ -9,7 +10,7 @@ import net.minecraftforge.event.village.WandererTradesEvent;
  * Valhelsia Core - net.valhelsia.valhelsia_core.helper.TradeHelper
  *
  * @author Valhelsia Team
- * @version 16.0.9
+ * @version 16.0.10
  * @since 2021-05-27
  */
 public class TradeHelper {
@@ -23,6 +24,18 @@ public class TradeHelper {
     public static void addRareWanderingTraderTrades(WandererTradesEvent event, VillagerTrades.ITrade... trades) {
         for (VillagerTrades.ITrade trade : trades) {
             event.getRareTrades().add(trade);
+        }
+    }
+
+    public static void addVillagerTrade(VillagerTradesEvent event, int level, VillagerTrades.ITrade... trades) {
+        for (VillagerTrades.ITrade trade : trades) {
+            event.getTrades().get(level).add(trade);
+        }
+    }
+
+    public static void addVillagerTrade(VillagerTradesEvent event, VillagerProfession profession, int level, VillagerTrades.ITrade... trades) {
+        if (event.getType() == profession) {
+            addVillagerTrade(event, level, trades);
         }
     }
 }
