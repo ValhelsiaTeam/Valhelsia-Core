38a437260c7b3ca48b1525d164b4e62541381edb
stal111 <stal111@gmx.de>
2021-10-02 14:44:37 +0200

    Fix FlammableHelper

--- src/main/java/net/valhelsia/valhelsia_core/core/registry/block/FlammableHelper.java
+++ src/main/java/net/valhelsia/valhelsia_core/core/registry/block/FlammableHelper.java
@@ -1,8 +1,8 @@
 package net.valhelsia.valhelsia_core.core.registry.block;
 
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FireBlock;
-import net.minecraft.world.level.block.ScaffoldingBlock;
 
 /**
  * Flammable Helper <br>
@@ -14,23 +14,23 @@ import net.minecraft.world.level.block.ScaffoldingBlock;
  */
 public class FlammableHelper {
 
-    public void register(ScaffoldingBlock block, int encouragement, int flammability) {
+    public void register(Block block, int encouragement, int flammability) {
          ((FireBlock) Blocks.FIRE).setFlammable(block, encouragement, flammability);
     }
 
-    public void registerLog(ScaffoldingBlock block) {
+    public void registerLog(Block block) {
         register(block, 5, 5);
     }
 
-    public void registerWoodenBlock(ScaffoldingBlock block) {
+    public void registerWoodenBlock(Block block) {
         register(block, 5, 20);
     }
 
-    public void registerLeaves(ScaffoldingBlock block) {
+    public void registerLeaves(Block block) {
         register(block, 30, 60);
     }
 
-    public void registerPlant(ScaffoldingBlock block) {
+    public void registerPlant(Block block) {
         register(block, 60, 100);
     }
 }
