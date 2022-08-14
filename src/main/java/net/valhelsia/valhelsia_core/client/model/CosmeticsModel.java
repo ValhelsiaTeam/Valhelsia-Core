5eb8165efd650a4d482b68a15109766e3e48e344
stal111 <stal111@gmx.de>
2022-08-14 12:09:32 +0200

    Allow any EntityModel to be a CosmeticModel

--- src/main/java/net/valhelsia/valhelsia_core/client/model/CosmeticsModel.java
+++ src/main/java/net/valhelsia/valhelsia_core/client/model/CosmeticsModel.java
@@ -2,20 +2,16 @@ package net.valhelsia.valhelsia_core.client.model;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.ListModel;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.world.entity.player.Player;
 
 /**
- * Cosmetics Model <br>
- * Valhelsia Core - net.valhelsia.valhelsia_core.client.model.CosmeticsModel
- *
  * @author Valhelsia Team
- * @version 1.17.1 - 0.1.2
  * @since 2021-10-25
  */
 public interface CosmeticsModel<T extends Player> {
-    ListModel<T> getModel();
+    <M extends EntityModel<T>> M getModel();
     void setPosition(PoseStack poseStack);
     default boolean translateToParent() {
         return true;
