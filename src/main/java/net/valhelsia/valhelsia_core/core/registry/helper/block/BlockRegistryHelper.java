d8c204eb9aecea5fdd626348bce1ff0dbd7c9e65
stal111 <stal111@gmx.de>
2022-12-26 21:45:43 +0100

    Update BlockRegistryHelper.java

--- src/main/java/net/valhelsia/valhelsia_core/core/registry/helper/block/BlockRegistryHelper.java
+++ src/main/java/net/valhelsia/valhelsia_core/core/registry/helper/block/BlockRegistryHelper.java
@@ -5,6 +5,7 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SignItem;
 import net.minecraft.world.level.block.Block;
@@ -21,10 +22,11 @@ import net.valhelsia.valhelsia_core.core.ValhelsiaCore;
 import net.valhelsia.valhelsia_core.core.registry.RegistryClass;
 import net.valhelsia.valhelsia_core.core.registry.helper.MappedRegistryHelper;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 /**
  * Block Registry Helper <br>
@@ -87,5 +89,29 @@ public class BlockRegistryHelper extends MappedRegistryHelper<Block> {
         standing.withItem(registryObject -> new SignItem(new Item.Properties().stacksTo(16), registryObject.get(), wall.get()));
 
         return Pair.of(standing, wall);
+    }
+
+    public <T extends Block> EnumMap<DyeColor, BlockRegistryObject<T>> createColorVariants(String name, Function<DyeColor, T> function) {
+        return Arrays.stream(DyeColor.values())
+                .map(color -> Pair.of(color, this.create(color.getName() + "_" + name, () -> function.apply(color))))
+                .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond, (o, o2) -> o, () -> new EnumMap<>(DyeColor.class)));
+    }
+
+    public <T extends Block> EnumMap<DyeColor, BlockRegistryObject<T>> createColorVariants(String name, Function<DyeColor, T> function, UnaryOperator<BlockRegistryObject<T>> unaryOperator) {
+        return Arrays.stream(DyeColor.values())
+                .map(color -> Pair.of(color, unaryOperator.apply(this.create(color.getName() + "_" + name, () -> function.apply(color)))))
+                .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond, (o, o2) -> o, () -> new EnumMap<>(DyeColor.class)));
+    }
+
+    public <T extends Block> EnumMap<DyeColor, BlockRegistryObject<T>> createColorVariants(UnaryOperator<String> name, Function<DyeColor, T> function) {
+        return Arrays.stream(DyeColor.values())
+                .map(color -> Pair.of(color, this.create(name.apply(color.getName()), () -> function.apply(color))))
+                .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond, (o, o2) -> o, () -> new EnumMap<>(DyeColor.class)));
+    }
+
+    public <T extends Block> EnumMap<DyeColor, BlockRegistryObject<T>> createColorVariants(UnaryOperator<String> name, Function<DyeColor, T> function, UnaryOperator<BlockRegistryObject<T>> unaryOperator) {
+        return Arrays.stream(DyeColor.values())
+                .map(color -> Pair.of(color, unaryOperator.apply(this.create(name.apply(color.getName()), () -> function.apply(color)))))
+                .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond, (o, o2) -> o, () -> new EnumMap<>(DyeColor.class)));
     }
 }
